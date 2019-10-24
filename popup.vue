beb561fe7c0df485ddae5fa78528213c2fdc2d9a
guan <guanzejian@shanghai.com.cn>
2019-10-24 11:02:27 +0800

    popup

---
+++ popup.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="popup" :class="{popupActive: popupActive}" >
+       <slot></slot> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:"popup",
+    data() {
+        return {
+            popupActive: false,
+        }
+    },    
+}
+</script>
+
+<style lang="scss" scoped>
+.popup{
+    width: 100%;
+    height: 40px;
+    position: absolute;
+    left: 0;
+    top: -40px;
+    line-height: 40px;
+    color: #fff;
+    background-color: #EF4F4F;
+    transition: all 0.5s ease;
+    opacity: 0;
+}
+.popupActive{
+    top: 0;
+    opacity: 1
+}
+</style>
